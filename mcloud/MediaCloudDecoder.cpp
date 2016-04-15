3df0d33c99352a587b2a630c5dc95146fb82abad
Julian Lobe <Julian.Lobe@hotmail.de>
2016-04-15 20:33:19 +0200

    Cosmetic fixes

--- mcloud/MediaCloudDecoder.cpp
+++ mcloud/MediaCloudDecoder.cpp
@@ -44,24 +44,20 @@ void Decoder::playAudioFile(File* file)
 	GstElement *play;
 	GstBus *bus;
 	
-	/* init GStreamer */
 	gst_init (0, 0);
 	loop = g_main_loop_new (NULL, FALSE);
-
-	/* set up */
+	
 	play = gst_element_factory_make ("playbin", "play");
 	g_object_set (G_OBJECT (play), "uri", str.str().c_str(), NULL);
-
+	
 	bus = gst_pipeline_get_bus (GST_PIPELINE (play));
 	gst_bus_add_watch (bus, bus_call, loop);
 	gst_object_unref (bus);
-
+	
 	gst_element_set_state (play, GST_STATE_PLAYING);
-
-	/* now run */
+	
 	g_main_loop_run (loop);
-
-	/* also clean up */
+	
 	gst_element_set_state (play, GST_STATE_NULL);
 	gst_object_unref (GST_OBJECT (play));
 }
@@ -71,27 +67,24 @@ gboolean Decoder::bus_call(GstBus *bus, GstMessage *msg, gpointer data)
 	GMainLoop *loop = (GMainLoop *) data;
 	
 	switch (GST_MESSAGE_TYPE (msg)) {
-		
 		case GST_MESSAGE_EOS:
-		g_print ("End of stream\n");
-		g_main_loop_quit (loop);
-		break;
+			g_print ("End of stream\n");
+			g_main_loop_quit (loop);
+			break;
+		case GST_MESSAGE_ERROR:
+			gchar  *debug;
+			GError *error;
+			
+			gst_message_parse_error (msg, &error, &debug);
+			g_free (debug);
 		
-		case GST_MESSAGE_ERROR: {
-		gchar  *debug;
-		GError *error;
-		
-		gst_message_parse_error (msg, &error, &debug);
-		g_free (debug);
-	
-		g_printerr ("Error: %s\n", error->message);
-		g_error_free (error);
+			g_printerr ("Error: %s\n", error->message);
+			g_error_free (error);
 
-		g_main_loop_quit (loop);
-		break;
-		}
+			g_main_loop_quit (loop);
+			break;
 		default:
-		break;
+			break;
 	}
 	
 	return TRUE;
