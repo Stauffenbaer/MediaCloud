4ebcc377d6a69f7b91965f127bf59b514e3fce16
Julian Lobe <Julian.Lobe@hotmail.de>
2016-04-18 16:21:07 +0200

    Started fixing dependencies

--- mcloud/MediaCloudFilesystem.h
+++ mcloud/MediaCloudFilesystem.h
@@ -38,13 +38,8 @@ SOFTWARE.
 #include <boost/filesystem.hpp>
 
 extern "C" {
-	#include <libavutil/mathematics.h>
-	#include <libavcodec/avcodec.h>
 	#include <libavformat/avformat.h>
 	#include <libavutil/avutil.h>
-	#include <libswscale/swscale.h>
-	
-	#include <ao/ao.h>
 }
 
 namespace MediaCloud {
